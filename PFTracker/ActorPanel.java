6cded1a431e2e5af5f143c05968973e990485036
Michael <MichaelWidler@yahoo.com>
2024-10-18 10:27:31 -0700

    Set layout for actorpanel.

--- PFTracker/ActorPanel.java
+++ PFTracker/ActorPanel.java
@@ -1,9 +1,11 @@
 package PFTracker;
 
+import java.util.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class ActorPanel extends JPanel
+public class ActorPanel extends JPanel implements ComponentListener
 {
 	private Actor actor;
    private JTextField nameF;
@@ -11,6 +13,7 @@ public class ActorPanel extends JPanel
    private JCheckBox[] actionCB;
    private JCheckBox reactionCB;
    private JTextField notesF;
+   private JPanel actionPanel;
 
 
 	public Actor getActor(){return actor;}
@@ -22,5 +25,61 @@ public class ActorPanel extends JPanel
    public ActorPanel()
    {
       super();
+      actor = new Actor();
+      setLayout(null);
+      
+      nameF = new JTextField("");
+      add(nameF);
+      
+      initF = new JTextField("");
+      add(initF);
+      
+      actionCB = new JCheckBox[3];
+      actionPanel = new JPanel();
+      actionPanel.setLayout(new GridLayout(1, actionCB.length));
+      for(int i = 0; i < actionCB.length; i++)
+      {
+         actionCB[i] = new JCheckBox();
+         actionPanel.add(actionCB[i]);
+      }
+      add(actionPanel);
+      
+      reactionCB = new JCheckBox();
+      add(reactionCB);
+      
+      notesF = new JTextField("");
+      add(notesF);
+      
+      setVisible(true);
+      resizeComponents();
    }
+   
+   public void resizeComponents()
+   {
+      int height = getHeight();
+      int widthSegment = getWidth() / 15;
+      int inset = 0;
+      
+      nameF.setSize(widthSegment * 4, height);
+      nameF.setLocation(widthSegment * 0, 0);
+      
+      initF.setSize(widthSegment, height);
+      initF.setLocation(widthSegment * 4, 0);
+      
+      actionPanel.setSize(widthSegment, height);
+      actionPanel.setLocation(widthSegment * 5, 0);
+      
+      reactionCB.setSize(widthSegment, height);
+      reactionCB.setLocation(widthSegment * 6, 0);
+      
+      notesF.setSize(widthSegment * 8, height);
+      notesF.setLocation(widthSegment * 7, 0);
+      repaint();
+   }
+   
+   // update metrics when resized
+   public void componentResized(ComponentEvent ce){resizeComponents();}
+   public void componentMoved(ComponentEvent ce){}
+   public void componentHidden(ComponentEvent ce){}
+   public void componentShown(ComponentEvent ce){repaint();}
 }
